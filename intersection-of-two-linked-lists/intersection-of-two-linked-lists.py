1917fa9c9d678783106cfc5a7fc475d8b22a6c99
Jungsu Han <saw1515@postech.ac.kr>
2021-09-07 14:08:02 +0900

    Time: 164 ms (63.68%), Space: 29.1 MB (99.24%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -7,17 +7,16 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         
+        # id가 같은 순간을 찾아야함 (단순히 val만 같으면 안됨..)
         if not headA or not headB :
             return None
         
-        
         curA = headA
         curB = headB
-        
         while curA is not curB :
             
             curA = headB if curA is None else curA.next
             curB = headA if curB is None else curB.next 
-        
+
         return curA
             
