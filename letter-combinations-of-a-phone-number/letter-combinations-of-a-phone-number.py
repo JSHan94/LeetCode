8a6b929e3789cd44e0dd8161bfaf98c58be9a3db
Jungsu Han <saw1515@postech.ac.kr>
2021-11-04 21:31:52 +0900

    Time: 32 ms (71.60%), Space: 14.1 MB (86.01%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -3,14 +3,16 @@ class Solution:
         num=[0,0,2,3,4,5,6,7,8,9]
         letter_list =  [0,0,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
         
-        res = [""] if digits else []
-        
-        for digit in digits:
-            letters = letter_list[int(digit)]
-            temp = []
-            for letter in letters:
-                for item in res : 
-                    temp.append(item + letter)
-            res = temp
-        
-        return res
+        res = []
+        n = len(digits)
+        def dfs(pos,path):
+            if len(path) == n:
+                return res.append(path)
+            if pos < n:
+                for i in letter_list[int(digits[pos])]:
+                    dfs(1+pos, path+i)
+                
+                
+        dfs(0,"")
+        return [] if not digits else res
+    
